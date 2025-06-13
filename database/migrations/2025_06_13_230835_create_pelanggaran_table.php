3d2b6a2d7fe734233be874fbff60c9162c03c172
miya-kurnia <kurniamiya83@gmail.com>
2025-06-14 06:16:01 +0700

    add tabel pelanggaran

---
+++ database/migrations/2025_06_13_230835_create_pelanggaran_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pelanggaran', function (Blueprint $table) {
+            $table->id();
+                        $table->string('jenis_kendaraan');       // Tambahan: Jenis kendaraan
+            $table->timestamp('waktu_pelanggaran');  // Tambahan: Waktu pelanggaran
+            $table->string('image');                 // Tambahan: Path gambar bukti
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pelanggaran');
+    }
+};
